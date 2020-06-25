8b362a7a8bcee90da595c1b3f8023edec6599377
Johan Medina <josmedinaca@unal.edu.co>
2020-06-25 13:56:03 -0500

    s

--- src/app/demo/pages/almuerzos/almuerzos.component.ts
+++ src/app/demo/pages/almuerzos/almuerzos.component.ts
@@ -6,6 +6,8 @@ import {MemoryUseDashServer2} from './chart/memory-use-dash-server2';
 import { VisitorGenderDashAnalytics } from './chart/visitor-gender-dash-analytics';
 import { ChartDB } from '../../dashboard/dash-server/chart/chart-data';
 
+declare const Highcharts: any;
+declare const sunburst: any;
 @Component({
   selector: 'app-almuerzos-page',
   templateUrl: './almuerzos-page.component.html',
@@ -32,8 +34,182 @@ export class AlmuerzosPageComponent implements OnInit {
 
 
 
-  ngOnInit(){}
-
+ngOnInit() {
+  this.sunburst();
+}
+sunburst(){
+  var data = [{
+    id: '0.0',
+    parent: '',
+    name: 'Subaccesos'
+  }, {
+    id: '1.1',
+    parent: '0.0',
+    name: 'PEAMA',
+    color: '#B5DDF4'
+  }, {
+    id: '1.3',
+    parent: '0.0',
+    name: 'PAES',
+    color:'#F1B6B6'
+  }, {
+    id: '1.4',
+    parent: '0.0',
+    name: 'PREGRADO',
+    color:'#CDF1B6',
+    
+  }, 
+  
+  /* PEAMA */
+  {
+    id: '2.1',
+    parent: '1.1',
+    name: 'Amazonia',
+    value: 604957438
+  }, 
+  
+  {
+    id: '2.5',
+    parent: '1.1',
+    name: 'Tumaco',
+    value: 190886311
+  },
+  
+  {
+    id: '2.3',
+    parent: '1.1',
+    name: 'Caribe',
+    value: 197553151
+  },
+  
+  {
+    id: '2.2',
+    parent: '1.1',
+    name: 'Bogota',
+    value: 181339988
+  },
+  
+  {
+    id: '2.4',
+    parent: '1.1',
+    name: 'Orinoquia',
+    value : 181339988
+  },
+  
+  
+  
+  /***********/
+  
+  /* PAES */
+  {
+    id: '2.9',
+    parent: '1.3',
+    name: 'Indigena',
+    value: 209288278
+  }
+  ,
+  
+  {
+    id: '2.8',
+    parent: '1.3',
+    name: 'Municipio',
+    value: 324459463
+  },
+  
+  {
+    id: '2.7',
+    parent: '1.3',
+    name: 'Poblacion Afro',
+    value: 129163276
+  },
+  
+  {
+    id: '2.6',
+    parent: '1.3',
+    name: 'Mejor Bachiller',
+    value: 111484636
+  },
+  
+  {
+    id: '2.71',
+    parent: '1.3',
+    name: 'Victimas',
+    value: 111484636
+  }, 
+  /***********/
+  
+  /* Asia */
+  
+  /***********/
+  
+  /* Pregrado */
+  {
+    id: '2.15',
+    parent: '1.4',
+    name: 'Regular pregrado',
+    value: 143989754
+  }
+  
+  ];
+  
+  
+  // Splice in transparent for the center circle
+  Highcharts.getOptions().colors.splice(0, 0, 'transparent');
+  
+  
+  Highcharts.chart('ch', {
+  
+    chart: {
+      height: '65%'
+    },
+  
+    title: {
+      text: 'Niveles de subacceso'
+    },
+    series: [{
+      type: "sunburst",
+      data: data,
+      allowDrillToNode: true,
+      cursor: 'pointer',
+      dataLabels: {
+        format: '{point.name}',
+        filter: {
+          property: 'innerArcLength',
+          operator: '>',
+          value: 16
+        },
+        rotationMode: 'circular'
+      },
+      levels: [{
+        level: 1,
+        levelIsConstant: false,
+        dataLabels: {
+          filter: {
+            property: 'outerArcLength',
+            operator: '>',
+            value: 64
+          }
+        }
+      }, {
+        level: 2,
+        colorByPoint: true
+      },
+      {
+        level: 3,
+        colorVariation: {
+          key: 'brightness',
+          to: -0.5
+        }
+      
+      }]
+  
+    }],
+    tooltip: {
+      headerFormat: "",
+      pointFormat: 'Existen <b>{point.value}</b> <b> estudiantes</b> '
+    }
+  });
+}
  
 
 
